0321890b034d272dca0a25c1003e3895bdb6084b
HITANSHU16 <hitanshusahni167@gmail.com>
2018-08-06 00:26:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
+class Square extends React.Component{
+render(){
+return(
+<button className = "square">
+Hit
+</button>	
+);
+}
+}
+class Board extends React.Component{
+renderSquare(i){
+return <Square />
+}
+render() {
+return (
+<div>
+<div className="board-row">
+{this.renderSquare(0)}
+{this.renderSquare(1)}
+{this.renderSquare(2)}
+</div>
+</div>
+);a
+}
+}
+class Game extends React.Component{
+render(){
+return(
+<div className="game">
+<div className="game-board">
+<Board />
+</div>
+</div>
+);
+}
+}
+ReactDOM.render(
+<Game />,
+document.getElementById('root')
+);
+registerServiceWorker();
